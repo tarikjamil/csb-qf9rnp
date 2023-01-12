4fbec7aee9917510163cf9d28d4d254c0d2cd7e5
Code Sandbox <hello@codesandbox.io>
2023-01-12 06:20:22 +0000

    Deployment from Codesandbox.

---
+++ package-script.js
@@ -0,0 +1,80 @@
+// Split text into characters using absolute positioning.
+let text;
+// Split the text up
+function runSplit() {
+  text = new SplitType(".scroll-animation", {
+    types: "lines",
+    lineClass: "split-line"
+  });
+}
+
+runSplit();
+// Update on window resize
+let windowWidth = $(window).innerWidth();
+window.addEventListener("resize", function () {
+  if (windowWidth !== $(window).innerWidth()) {
+    windowWidth = $(window).innerWidth();
+    text.revert();
+    runSplit();
+  }
+});
+
+let text2;
+// Split the text up
+function runSplit2() {
+  text2 = new SplitType(".is--hero-split", {
+    types: "lines",
+    lineClass: "loading-animation"
+  });
+}
+
+runSplit2();
+// Update on window resize
+let windowWidth2 = $(window).innerWidth();
+window.addEventListener("resize", function () {
+  if (windowWidth2 !== $(window).innerWidth()) {
+    windowWidth2 = $(window).innerWidth();
+    text2.revert();
+    runSplit2();
+  }
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+$(".scroll-animation").each(function (index) {
+  let triggerElement = $(this);
+  // let myText = $(this).find(".split-text");
+  let targetElement = $(this).find(".split-line");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      // trigger element - viewport
+      start: "top bottom",
+      end: "bottom top"
+    }
+  });
+  tl.from(targetElement, {
+    duration: 1,
+    delay: 0.3,
+    y: "50rem",
+    opacity: 0,
+    ease: "Quint.easeOut",
+    stagger: {
+      amount: 0.1,
+      from: "0"
+    }
+  });
+});
+
+// On Page Load
+function pageLoad() {
+  let tl = gsap.timeline();
+  tl.from(".loading-animation", {
+    y: "200%",
+    stagger: { each: 0.1, from: "start" },
+    ease: "Quint.easeOut",
+    duration: 1
+  });
+}
+pageLoad();
---
+++ product.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".slider1", {
+    type: "slider",
+    perPage: 2,
+    perMove: 1,
+    pagination: false,
+    gap: "48rem",
+    breakpoints: {
+      991: {
+        // Tablet
+        perPage: 1,
+        gap: "24rem",
+        arrows: false
+      },
+      767: {
+        // Mobile Landscape
+        perPage: 1,
+        gap: "24rem",
+        arrows: false
+      },
+      479: {
+        // Mobile Portrait
+        perPage: 1,
+        gap: "24rem",
+        arrows: false
+      }
+    }
+  });
+  splide.mount();
+});
