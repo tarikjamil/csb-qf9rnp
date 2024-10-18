bf1c39dd6d06d3e9b669328c10ca29641e30bb7d
tarikjamil <tarikjamill@gmail.com>
2024-10-18 14:21:50 +0100

    faq

--- script.js
+++ script.js
@@ -324,3 +324,32 @@ $(document).ready(function () {
     }
   });
 });
+
+// ------------------ accordion ------------------ //
+
+$(".faq--question").on("click", function () {
+  // Close other accordions when opening new one
+  if (!$(this).hasClass("open")) {
+    $(".faq--question.open").click();
+  }
+  // Save the sibling of the toggle we clicked on
+  let sibling = $(this).siblings(".faq--response");
+  let animationDuration = 500;
+
+  if ($(this).hasClass("open")) {
+    // Close the content div if already open
+    sibling.animate({ height: "0px" }, animationDuration);
+  } else {
+    // Open the content div if already closed
+    sibling.css("height", "auto");
+    let autoHeight = sibling.height();
+    sibling.css("height", "0px");
+    sibling.animate({ height: autoHeight }, animationDuration, () => {
+      sibling.css("height", "auto");
+
+      // Scroll the page to the accordion, leaving 200 pixels from the top
+    });
+  }
+  // Open and close the toggle div
+  $(this).toggleClass("open");
+});
